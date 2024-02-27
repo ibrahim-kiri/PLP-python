81829ee9ca343bf52f61e30ca95a7dbfc4507693
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-02-27 10:38:04 +0300

    first commit

---
+++ lesson2.py
@@ -0,0 +1,101 @@
+site_name = "Power Learn Project2024"
+print(site_name)
+site_name = "Ubuntu"
+print(site_name)
+
+a, b, c = 3.2, 5, "Hello"
+print(a)
+print(b)
+print(c)
+
+num1 = 5
+print(num1, 'is of type', type(num1))
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
+
+
+languages = ["Swift", "Java", "Python"]
+print(languages[0])
+print(languages[1])
+
+
+product = ('Microsoft', 'Xbox', 499.99)
+print(product[0])
+print(product[1])
+
+
+name = 'Python'
+print(name)
+message = 'Python for beginners'
+print(message)
+
+student_id = {112, 114, 116, 118, 115}
+print(student_id)
+print(type(student_id))
+
+capital_city = {'Nepal': 'Kathmandu', 'Italy': 'Rome', 'England': 'London'}
+print(capital_city)
+print(capital_city['Nepal'])
+print(capital_city['England'])
+
+num1 = 6
+num2 = 9
+print('This is output')
+
+interger_number = 123
+float_number = 1.23
+new_number = interger_number + float_number
+print("Value:",new_number)
+print("Data Type:",type(new_number))
+
+num_string = '12'
+num_interger = 23
+print("Data type of num_string before Type Casting:",type(num_string))
+num_string = int(num_string)
+print("Data type of num_string after Type Casting:",type(num_string))
+num_sum = num_interger + num_string
+print("Sum:",num_sum)
+print("Data type of num_sum:",type(num_sum))
+
+sub = 10 - 5
+# var x = 5
+
+a = 5
+b = 2
+print (a > b)
+
+a = 5
+b = 6
+print((a > 2) and (b >= 6))
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)
+# check if 'hello' is present in x string
+print('hello' not in x)
+# check if '1' key is present in y
+print(1 in y)
+# check if 'a' key is present in y
+print('a' in y)
+
+print('Good Morning!')
+print('Its rainy today')
+
+print('Good Morning!', end= ' ')
+print('It is rainy today')
+
+number = -10.6
+name = "Power LP"
+print(5)
+print(number)
+print(name)
+
+print('PowerLP is ' + 'awesome.')
+
+num = input('Enter a number: ')
+print('You Entered:', num)
+print('Data type of num:', type(num))
